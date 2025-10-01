faea1caf16efe678b1959f54daba2a010b94972c
HoTienQuoc <hotienquoc0429@gmail.com>
2025-10-01 22:57:23 +0700

    Refactor project structure: update lint command, add path aliases, and remove unused pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hello from "@/app/components/hello";
 
 export default function Home() {
--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-
-export default function About() {
-  console.log("What am I doing here? -- Server/Client");
-  return (
-    <div>About</div>
-  );
-}
